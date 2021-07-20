80b23a669a610b4e03d7760938aca1a5fef7389c
Mejia <fmejia@usgs.gov>
2021-07-20 16:55:08 -0700

    this is revision 2

--- text_mining.R
+++ text_mining.R
@@ -3,7 +3,7 @@
 ##Frequency table for a selected list of terms: water temperature, thermal refuge, refugia, coldwater refuge, salmonid, trout, salmon
 
 
-setwd('C:\\Users\\fmejia\\1_Francine\\1_Francine from C\\SESYNC\\SESYNC proposal coldwater refuges\\Data for class')
+setwd('C:\\Users\\fmejia\\1_Francine\\1_Francine from C\\SESYNC\\SESYNC proposal coldwater refuges\\Data for class\\Oregon')
 
 getwd()
 
@@ -30,13 +30,86 @@ dtm <- DocumentTermMatrix(corp)
 inspect(dtm)  ##too many words are prepositions, I want to search for nouns!
 
 
-####lifted from internet###
+###cleaning files: getting rid of white spaces, endings and stopwords
+
+
+install.packages('magrittr')
+
+library(magrittr)
+dtm_words <- corp %>%
+  tm_map(removePunctuation) %>%
+  tm_map(removeNumbers) %>%
+  tm_map(stripWhitespace)  %>%
+ # tm_map(stemDocument) %>% ### didn't use so words don't lose ending like temperature
+  tm_map(removeWords, stopwords("english"))
+
+
+dtmw <- DocumentTermMatrix(dtm_words)
+inspect(dtmw)  # checking what matrix looks like
+
+
+##finding frequent terms to trim length of words
+
+library(tidytext)
+library(dplyr)
+dtt <- tidy(dtmw)
+words <- dtt %>%
+  group_by(term) %>%
+  summarise(
+    n = n(),
+    total = sum(count)) %>%
+  mutate(nchar = nchar(term))
+
+### Plotting the number of characters
+
+library(ggplot2)
+ggplot(words, aes(x = nchar)) +
+  geom_histogram(binwidth = 1)
+
+
+####### trimmingthe number of characters in words
+
+dtt_trimmed <- words %>%
+  filter(
+    nchar < 16,
+    n > 1,
+    total > 3) %>%
+  select(term) %>%
+  inner_join(dtt)
+
+dtm_trimmed <- dtt_trimmed %>%
+  cast_dtm(document, term, count)
+
+dtm_trimmed
+
+
+### develop word associations with key words such temperatur and refugia  
+#####using tm package finding associations --code from class
+
+word_assoc <- findAssocs(dtm_trimmed, 'temperature', 0.95)
+word_assoc <- data.frame(
+  word = names(word_assoc[[1]]),
+  assoc = word_assoc,
+  row.names = NULL)
+
+###visualization of word association
+
+library(ggwordcloud)
+ggplot(word_assoc,
+       aes(label = word, size = temperature)) +
+  geom_text_wordcloud_area()
+
+
+
+####code lifted from internet### using quanteda package
 
 devtools::install_github("kbenoit/quanteda", quiet = TRUE)
 require(quanteda)
 data(files, package = "tm") ## this line is not working..
-mycorpus <- corpus(files)
+mycorpus <- corpus(dtm_words)
 textp<- tokens(mycorpus, what="word")
-kwic(textp, "water temperature")
+kwic(textp, "temperatur")
+
+
 
 
